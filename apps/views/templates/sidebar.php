18e08df537cf6e3ac906bca07c50c20f499b44d1
Faizal fernandi <faizal.fernandi@togu.co.id>
2020-12-02 23:56:30 +0700

    kdf

--- apps/views/templates/sidebar.php
+++ apps/views/templates/sidebar.php
@@ -1,3 +1,4 @@
+<?php if(!empty($_SESSION['userdata'])) : ?>
 <div id="layoutSidenav_nav">
                 <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
                     <div class="sb-sidenav-menu">
@@ -33,3 +34,40 @@
                     </div>
                 </nav>
             </div>
+<?php else : ?>
+
+    <div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                    <div class="sb-sidenav-menu">
+                        <div class="nav">
+                            <div class="sb-sidenav-menu-heading">MAIN MENU</div>
+                            <a class="nav-link" href="<?= BASE_URL; ?>">
+                                <div class="sb-nav-link-icon"><i class="fas fa-fw fa-tachometer-alt"></i></div>
+                                Dashboard
+                            </a>
+                            <a class="nav-link" href="<?= BASE_URL; ?>penerimabantuan">
+                                <div class="sb-nav-link-icon"><i class="fas fa-fw fa-hands-helping"></i></div>
+                                Penerima bantuan
+                            </a>
+                            <a class="nav-link" href="<?= BASE_URL; ?>catatanbantuan">
+                                <div class="sb-nav-link-icon"><i class="fas fa-fw fa-clipboard-list"></i></div>
+                                Catatan bantuan
+                            </a>
+                            <a class="nav-link" href="<?= BASE_URL; ?>Datapenduduk">
+                                <div class="sb-nav-link-icon"><i class="fas fa-fw fa-clipboard-list"></i></div>
+                                Data penduduk
+                            </a>
+                            <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
+                                <nav class="sb-sidenav-menu-nested nav">
+                                <a class="nav-link" href="<?= BASE_URL; ?>Kategoribantuan">Kategori bantuan</a>
+                                    <a class="nav-link" href="<?= BASE_URL; ?>Datapenduduk">Data penduduk</a>
+                                    <a class="nav-link" href="<?= BASE_URL; ?>Pekerjaan">Data pekerjaan</a>
+                                    <a class="nav-link" href="<?= BASE_URL; ?>Rules">Pengurus</a>
+                                </nav>
+                            </div>
+                           
+                        </div>
+                    </div>
+                </nav>
+            </div>
+<?php endif; ?>
