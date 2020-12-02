cf258908b0d583e7caa7fee03a7a5cf53b7b38a7
Faizal fernandi <faizal.fernandi@togu.co.id>
2020-12-02 23:43:54 +0700

    add database

--- pengajuan_bantuan.sql
+++ pengajuan_bantuan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Nov 09, 2020 at 02:58 AM
+-- Generation Time: Dec 02, 2020 at 04:43 PM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.3.12
 
@@ -32,12 +32,21 @@ DROP TABLE IF EXISTS `bantuan`;
 CREATE TABLE IF NOT EXISTS `bantuan` (
   `id_bantuan` int(11) NOT NULL AUTO_INCREMENT,
   `id_kategori_bantuan` int(11) NOT NULL,
-  `waktu_bantuan` int(11) NOT NULL,
+  `periode` int(11) NOT NULL,
   `description` text NOT NULL,
   `created_at` int(11) NOT NULL,
   `created_by` int(11) NOT NULL,
   PRIMARY KEY (`id_bantuan`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `bantuan`
+--
+
+INSERT INTO `bantuan` (`id_bantuan`, `id_kategori_bantuan`, `periode`, `description`, `created_at`, `created_by`) VALUES
+(3, 4, 1606176000, 'bantuan raskin', 1606156347, 3),
+(2, 1, 1608681600, 'bantuan covid 19', 1606067140, 3),
+(4, 1, 1606089600, 'covid', 1606156384, 3);
 
 -- --------------------------------------------------------
 
@@ -53,7 +62,14 @@ CREATE TABLE IF NOT EXISTS `bukti_terima` (
   `created_at` int(11) NOT NULL,
   `created_by` int(11) NOT NULL,
   PRIMARY KEY (`id_bukti_terima`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `bukti_terima`
+--
+
+INSERT INTO `bukti_terima` (`id_bukti_terima`, `name`, `source`, `created_at`, `created_by`) VALUES
+(2, 'gunawan3.jpeg', 'assets/bukti_terima', 1606762186, 3);
 
 -- --------------------------------------------------------
 
@@ -69,7 +85,15 @@ CREATE TABLE IF NOT EXISTS `kategori_bantuan` (
   `created_at` int(11) NOT NULL,
   `created_by` int(11) NOT NULL,
   PRIMARY KEY (`id_kategori_bantuan`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `kategori_bantuan`
+--
+
+INSERT INTO `kategori_bantuan` (`id_kategori_bantuan`, `name`, `description`, `created_at`, `created_by`) VALUES
+(1, 'COVID-19', 'bantuan masa pandemi covid-19', 1605808074, 3),
+(4, 'Raskin', 'bantuan raskin', 1606144832, 3);
 
 -- --------------------------------------------------------
 
@@ -86,21 +110,26 @@ CREATE TABLE IF NOT EXISTS `keluarga` (
   `kepala_keluarga` varchar(100) NOT NULL,
   `jumlah_keluarga` int(11) NOT NULL,
   `jumlah_anak` int(11) NOT NULL,
-  `rt` int(11) NOT NULL,
-  `rw` int(11) NOT NULL,
-  `alamat` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
+  `rt` varchar(11) NOT NULL,
+  `rw` varchar(11) NOT NULL,
+  `alamat` text NOT NULL,
+  `lat` double NOT NULL,
+  `lng` double NOT NULL,
+  `pass` varchar(255) NOT NULL,
   `created_at` int(11) NOT NULL,
   `created_by` int(11) NOT NULL,
   PRIMARY KEY (`id_keluarga`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `keluarga`
 --
 
-INSERT INTO `keluarga` (`id_keluarga`, `rules_id`, `no_kk`, `id_pekerjaan`, `kepala_keluarga`, `jumlah_keluarga`, `jumlah_anak`, `rt`, `rw`, `alamat`, `password`, `created_at`, `created_by`) VALUES
-(1, 1, '3326080802990021', 1, 'Bejo', 3, 1, 1, 2, 'jl.jendral sudirman, pegaden tengah', '202cb962ac59075b964b07152d234b70', 1604597002, 1);
+INSERT INTO `keluarga` (`id_keluarga`, `rules_id`, `no_kk`, `id_pekerjaan`, `kepala_keluarga`, `jumlah_keluarga`, `jumlah_anak`, `rt`, `rw`, `alamat`, `lat`, `lng`, `pass`, `created_at`, `created_by`) VALUES
+(3, 1, '8973874652928276', 1, 'tejo', 4, 2, '003', '004', 'jl.tentara pelajar desa kutorejo', 0, 0, '202cb962ac59075b964b07152d234b70', 1605550314, 3),
+(24, 2, '0987876545678987', 35, 'kanto', 5, 3, '001', '003', 'jl.jendral', 0, 0, '202cb962ac59075b964b07152d234b70', 1605638101, 3),
+(23, 2, '0987654323456789', 41, 'gunawan', 5, 3, '001', '004', 'jl.tentara pelajar', -6.972830797936966, 109.6213398581665, '202cb962ac59075b964b07152d234b70', 1606854665, 3),
+(25, 2, '4393837363546478', 41, 'suwarno', 5, 3, '003', '002', 'pegaden tengah, rt 003 rw 002', -6.973629474627812, 109.62455850898436, '202cb962ac59075b964b07152d234b70', 1606853314, 3);
 
 -- --------------------------------------------------------
 
@@ -116,14 +145,14 @@ CREATE TABLE IF NOT EXISTS `pekerjaan` (
   `created_at` int(11) NOT NULL,
   `created_by` int(11) NOT NULL,
   PRIMARY KEY (`id_pekerjaan`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=42 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `pekerjaan`
 --
 
 INSERT INTO `pekerjaan` (`id_pekerjaan`, `name`, `description`, `created_at`, `created_by`) VALUES
-(1, 'pengangguran', 'sudah dipecat', 1604597002, 1);
+(41, 'buruh', 'buruh', 1605640138, 3);
 
 -- --------------------------------------------------------
 
@@ -142,7 +171,14 @@ CREATE TABLE IF NOT EXISTS `penerima_bantuan` (
   `created_at` int(11) NOT NULL,
   `created_by` int(11) NOT NULL,
   PRIMARY KEY (`id_penerima`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `penerima_bantuan`
+--
+
+INSERT INTO `penerima_bantuan` (`id_penerima`, `id_bantuan`, `id_keluarga`, `status_terima`, `id_bukti_terima`, `tgl_terima`, `created_at`, `created_by`) VALUES
+(2, 3, 23, 1, 2, 1606780800, 1606762186, 3);
 
 -- --------------------------------------------------------
 
